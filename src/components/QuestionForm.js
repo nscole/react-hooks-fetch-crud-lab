9605523c7d54550f0be83c2d96ba1879e0de79ec
Naomi Cole <naomisarahcole@gmail.com>
2022-08-11 16:49:23 +1000

    COmpleted assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({onAddNewQuestion}) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -19,7 +19,20 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const {answer1, answer2, answer3, answer4, ...newData} = formData
+    const formatedFormData = {
+      ...newData, answers:[formData.answer1, formData.answer2, formData.answer3, formData.answer4]
+    }
+
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formatedFormData)
+    })
+    .then(r=>r.json())
+    .then(data=>onAddNewQuestion(data))
   }
 
   return (
